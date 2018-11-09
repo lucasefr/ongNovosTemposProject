88afb0089fda587cfbdb8b9aef4c13d6195ee636
Lucas Emanuel <lucasefr@gmail.com>
2018-11-09 00:10:44 -0200

    First Commit

---
+++ src/main/java/com/ong/novostempos/OngNovosTemposProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ong.novostempos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngNovosTemposProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngNovosTemposProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ong/novostempos/OngNovosTemposProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ong.novostempos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OngNovosTemposProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
